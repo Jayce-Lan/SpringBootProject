07dbc11b036099d2bd54bf9807b6fed92e2030e7
jayce <jayce404@foxmail.com>
2021-11-21 12:59:04 +0800

    Spring Boot MVC

---
+++ SpringBoot2.x/chapter1/src/main/java/com/springmvc/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.springmvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("indexcontroller")
+public class IndexController {
+    @RequestMapping("/test")
+    public Map<String, String> testMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        return map;
+    }
+
+}

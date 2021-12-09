8204f64f0cf0ec787944db0eb725a31f23d2aa85
lanjiesi <jayce404@foxmail.com>
2021-12-09 11:05:38 +0800

    文件上传下载

---
+++ SpringBoot2.x/chapter3-up-download/src/main/java/com/example/Chapter3UpDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter3UpDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter3UpDownloadApplication.class, args);
+	}
+
+}

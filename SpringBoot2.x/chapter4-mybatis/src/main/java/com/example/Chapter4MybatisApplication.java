a9ff98f171d11c49cb01c2d60fa02afdb6584f0b
lanjiesi <jayce404@foxmail.com>
2022-02-07 16:20:00 +0800

    整合mybatis

---
+++ SpringBoot2.x/chapter4-mybatis/src/main/java/com/example/Chapter4MybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter4MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter4MybatisApplication.class, args);
+	}
+
+}
---
+++ SpringBoot2.x/chapter4-mybatis/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import com.example.mapper.UserMapper;
+import com.example.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/queryUserList")
+    public List<User> queryUserList() {
+        return userMapper.queryUserList();
+    }
+}

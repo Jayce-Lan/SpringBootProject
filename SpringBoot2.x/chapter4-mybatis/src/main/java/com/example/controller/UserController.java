237325ed8ca7f06136fadce5a6f0f278f4d48cbf
lanjiesi <jayce404@foxmail.com>
2022-02-08 19:06:16 +0800

    对增删改查做一些修改

--- SpringBoot2.x/chapter4-mybatis/src/main/java/com/example/controller/UserController.java
+++ SpringBoot2.x/chapter4-mybatis/src/main/java/com/example/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.mapper.UserMapper;
 import com.example.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin // 解决跨域问题
 public class UserController {
     @Autowired
     private UserMapper userMapper;
@@ -19,9 +21,12 @@ public class UserController {
         return userMapper.queryUserList();
     }
 
-    @PostMapping("/addUser1")
-    public String addUser(User user) {
-        System.err.println(user);
+    @PostMapping("/addUser")
+    public String addUser(User user) throws Exception {
+        int count = userMapper.addUser(user);
+        if (count != 1) {
+            throw new Exception("错误");
+        }
         return "ok";
     }
 
@@ -33,6 +38,25 @@ public class UserController {
         int count = userMapper.addUser(user);
         if (count != 1) {
             throw new Exception("错误！");
+        }
+        return "ok";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(User user) throws Exception {
+        int count = userMapper.updateUser(user);
+        if (count != 1) {
+            throw new Exception("错误");
+        }
+        return "ok";
+    }
+
+    @PostMapping("/deleteUser")
+    public String deleteUser(User user) throws Exception {
+        int id = user.getId();
+        int count = userMapper.deleteUserById(id);
+        if (count != 1) {
+            throw new Exception("错误");
         }
         return "ok";
     }

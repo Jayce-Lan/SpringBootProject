44eed9338f22cc9800248dd17429cf4a7bcf7276
lanjiesi <jayce404@foxmail.com>
2022-02-08 15:02:58 +0800

    对增删改查做一些修改

--- SpringBoot2.x/chapter4-mybatis/src/main/java/com/example/controller/UserController.java
+++ SpringBoot2.x/chapter4-mybatis/src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.mapper.UserMapper;
 import com.example.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,23 @@ public class UserController {
     @GetMapping("/queryUserList")
     public List<User> queryUserList() {
         return userMapper.queryUserList();
+    }
+
+    @PostMapping("/addUser1")
+    public String addUser(User user) {
+        System.err.println(user);
+        return "ok";
+    }
+
+    @GetMapping("/addUser2")
+    public String addUser2() throws Exception {
+        User user = new User();
+        user.setName("李四");
+        user.setPwd("qwert");
+        int count = userMapper.addUser(user);
+        if (count != 1) {
+            throw new Exception("错误！");
+        }
+        return "ok";
     }
 }

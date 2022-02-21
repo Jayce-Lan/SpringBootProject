dbd4f20d12cd07978cbc2c486f3f1ff3e1a16a74
lanjiesi <jayce404@foxmail.com>
2022-02-21 15:34:14 +0800

    Spring Boot整合Redis

---
+++ SpringBoot2.x/chapter6-redis/src/main/java/com/redis/Chapter6RedisApplication.java
@@ -0,0 +1,13 @@
+package com.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter6RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter6RedisApplication.class, args);
+	}
+
+}

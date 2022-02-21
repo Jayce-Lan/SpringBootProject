0bc2d2c774d852e6ed31de2c3352532ba0518ee2
lanjiesi <jayce404@foxmail.com>
2022-02-21 11:07:12 +0800

    缓存

---
+++ SpringBoot2.x/chapter6-cache/src/main/java/com/cache/Chapter6CacheApplication.java
@@ -0,0 +1,13 @@
+package com.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter6CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter6CacheApplication.class, args);
+	}
+
+}

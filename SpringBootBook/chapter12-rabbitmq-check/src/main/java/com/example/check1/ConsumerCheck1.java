bb64929a59dbbefa540997555b21d5a215635bc5
lanjiesi <jayce404@foxmail.com>
2022-11-22 11:09:51 +0800

    消息队列的ACK和NACK机制

--- SpringBootBook/chapter12-rabbitmq-check/src/main/java/com/example/check1/ConsumerCheck1.java
+++ SpringBootBook/chapter12-rabbitmq-check/src/main/java/com/example/check1/ConsumerCheck1.java
@@ -22,7 +22,7 @@ public class ConsumerCheck1 {
      * @param channel
      * @throws IOException
      */
-    @RabbitListener(queues = RabbitMQConst.QUEUE_1)
+//    @RabbitListener(queues = RabbitMQConst.QUEUE_1)
     public void receiveConfirm(Message message, Channel channel) throws IOException {
         try {
             // 模拟接收失败
@@ -33,6 +33,32 @@ public class ConsumerCheck1 {
             // 两个布尔值，若第二个设为fales，则丢弃该消息；若设为true，则返回给队列
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
             log.error("消费失败 >>>>>>>>> 我此次将返回给队列");
+        }
+    }
+
+    /**
+     * 消费端ACK和NACK机制
+     * 该机制不回堆积消息
+     *
+     * @param message
+     * @param channel
+     */
+    @RabbitListener(queues = RabbitMQConst.QUEUE_1)
+    public void process(Message message, Channel channel) throws IOException {
+        try {
+            log.info("正常接收到信息 ACK >>>>>>>>>> {}", new String(message.getBody()));
+            int a = 1 / 0;
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            if (message.getMessageProperties().getRedelivered()) {
+                log.error("消息已重复处理失败 >>>>>>>>>> 拒绝再次接收！");
+                // 拒绝消息，requeue = false 白哦是不再重新进入队列，如果配置类死信队列，则进入死信队列
+                channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+            } else {
+                log.error("消息处理失败即将再次返回队列中处理 >>>>>>>> NACK");
+                // requeue = true 时重新进入队列
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            }
         }
     }
 }

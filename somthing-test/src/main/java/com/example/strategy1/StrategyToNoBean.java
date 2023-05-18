20b965021baba092570624c2201147d3464e68a0
lanjiesi <jayce404@foxmail.com>
2023-05-18 16:43:35 +0800

    普通情况下的策略模式

---
+++ somthing-test/src/main/java/com/example/strategy1/StrategyToNoBean.java
@@ -0,0 +1,9 @@
+package com.example.strategy1;
+
+/**
+ * 简单的策略模式（非Spring）
+ */
+public interface StrategyToNoBean {
+    String flag();
+    void process();
+}
---
+++ somthing-test/src/test/java/com/example/strategy/TestStrategy1.java
@@ -0,0 +1,32 @@
+package com.example.strategy;
+
+import com.example.strategy1.StrategyToNoBean;
+import com.example.strategy1.impl.StrategyToNoBean1Impl;
+import com.example.strategy1.impl.StrategyToNoBean2Impl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 简单策略模式的测试类
+ */
+@SpringBootTest
+@Slf4j
+public class TestStrategy1 {
+    private static final List<StrategyToNoBean> STRATEGIES = Arrays.asList(new StrategyToNoBean1Impl(), new StrategyToNoBean2Impl());
+    private static Map<String, StrategyToNoBean> STRATEGY_MAP;
+
+    static {
+        STRATEGY_MAP = STRATEGIES.stream().collect(Collectors.toMap(StrategyToNoBean :: flag, s -> s));
+    }
+
+    @Test
+    void test1() {
+        STRATEGY_MAP.get("2").process();
+    }
+}

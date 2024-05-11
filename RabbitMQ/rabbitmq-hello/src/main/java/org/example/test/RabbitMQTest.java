4c2cf46e9259469bc3ce16d00e58c7c514f8f171
lanjs <jayce404@foxmail.com>
2024-05-11 10:31:06 +0800

    交换机的概念

---
+++ RabbitMQ/rabbitmq-hello/src/main/java/org/example/test/RabbitMQTest.java
@@ -0,0 +1,23 @@
+package org.example.test;
+
+import org.example.util.RabbitMQTestUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 用于测试一些方法的类
+ */
+public class RabbitMQTest {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        RabbitMQTest rabbitMQTest = new RabbitMQTest();
+        rabbitMQTest.testGetQueue();
+    }
+
+    /**
+     * 测试Rabbit MQ官方提供的创建临时队列的方法
+     */
+    private void testGetQueue() throws IOException, TimeoutException {
+        System.out.println(RabbitMQTestUtils.getChannel().queueDeclare().getQueue());
+    }
+}
